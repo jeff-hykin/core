2aef7799d10d335af2d1dd11414d19037abe7d57
Jeff Hykin <jeff.hykin@gmail.com>
2025-04-20 16:15:41 -0500

    add non-global option

---
+++ src/all.ts
@@ -0,0 +1,12 @@
+export * from './backends/index.js';
+export * from './config.js';
+export * from './context.js';
+export * from './internal/index.js';
+export * from './mixins/index.js';
+export * from './vfs/stats.js';
+export * from './utils.js';
+export { mounts } from './vfs/shared.js';
+export * from './vfs/index.js';
+export { fs };
+import * as fs from './vfs/index.js';
+export default fs;
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,13 @@
-export * from './backends/index.js';
-export * from './config.js';
-export * from './context.js';
-export * from './internal/index.js';
-export * from './mixins/index.js';
-export * from './vfs/stats.js';
-export * from './utils.js';
-export { mounts } from './vfs/shared.js';
-export * from './vfs/index.js';
-export { fs };
-import * as fs from './vfs/index.js';
-export default fs;
+export * from './all.js';
+import all from './all.js';
+export default all
 
 declare const globalThis: {
 	/**
 	 * Global VFS. Do not use unless absolutely needed.
 	 * @hidden
 	 */
-	__zenfs__: typeof fs;
+	__zenfs__: typeof all;
 };
 
-globalThis.__zenfs__ = fs;
+globalThis.__zenfs__ = all;
